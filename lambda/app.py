e1bdff1918275b7015d3169c435df26c597f7cf6
Kingsly Roche <kingslyroche@gmail.com>
2021-04-30 15:22:40 +0530

    test_v1

--- lambda/app.py
+++ lambda/app.py
@@ -28,9 +28,8 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": responseBody,
         "headers": {
-            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,OPTIONS" 
+            "Access-Control-Allow-Methods": "GET" 
         }
     }
 
